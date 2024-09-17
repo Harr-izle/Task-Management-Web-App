d8b8f883fc483c015b077c26e42b339e6f4db924
Harr-izle <harrietbuadee@gmail.com>
2024-09-17 17:04:04 +0000

    styles side bar and updates partials

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ThemeComponent } from "../theme/theme.component";
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [ThemeComponent],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
