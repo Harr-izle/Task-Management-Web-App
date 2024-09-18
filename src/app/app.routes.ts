a6bf7408dc221bc57cfea0c1214f0897d4b966bd
Harr-izle <harrietbuadee@gmail.com>
2024-09-18 10:14:06 +0000

    restyles sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'board',
+        loadComponent: () =>
+            import('./components/board/board.component').then((m) => m.BoardComponent),
+    },
+    {
+        path: '',
+        redirectTo: 'board',
+        pathMatch: 'full',
+    },
+];
