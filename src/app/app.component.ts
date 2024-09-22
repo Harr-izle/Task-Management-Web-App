bd20c8db8cb6d0ccf1097f75cc8b0d657946fb09
Harr-izle <harrietbuadee@gmail.com>
2024-09-22 01:43:21 +0000

    creates component for delete modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
+import { DeleteModalComponent } from "./components/delete-modal/delete-modal.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, SidebarComponent],
+  imports: [RouterOutlet, HeaderComponent, SidebarComponent, DeleteModalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
